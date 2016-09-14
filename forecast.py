450bd19046b00fe8fca108465881af881f7ece3c
Daniel Miller <daniel@zoox.com>
2016-09-13 22:46:50 -0700

    Add UNITS, LOCATION, and TAGS vars
    And make it shut down gracefully on SIGTERM

--- forecast.py
+++ forecast.py
@@ -4,11 +4,22 @@ from __future__ import print_function
 from influxdb import InfluxDBClient
 import json
 import os
+import signal
 import subprocess
 import sys
 import time
 import urllib
 import urllib2
+
+class GracefulKiller:
+    kill_now = False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self,signum, frame):
+        print("Exiting gracefully")
+        self.kill_now = True
 
 def isfloat(value):
     try:
@@ -17,11 +28,28 @@ def isfloat(value):
     except ValueError:
         return False
 
-def record_weather(api_key, latitude, longitude, db_addr, db_port, db_name, period, units):
-    url = "https://api.forecast.io/forecast/%s/%s?units=%s&exclude=minutely,hourly,daily,alerts,flags" % (api_key, units, ",".join([latitude, longitude]))
-    client = InfluxDBClient(db_addr, db_port, 'root', 'root', db_name)
+def record_weather(api_key, latitude, longitude, db_addr, db_port, db_name, period, units, location, tags):
+    killer = GracefulKiller()
 
-    while True:
+    # Bulid URL for Forecast.io request
+    url = "https://api.forecast.io/forecast/{api_key}/{latitude},{longitude}" \
+            "?units={units}&exclude=minutely,hourly,daily,alerts,flags"
+    url = url.format(api_key=api_key,
+                     units=units,
+                     latitude=latitude,
+                     longitude=longitude)
+
+    # Parse given tags, and add location
+    json_acceptable_string = tags.replace("'", "\"")
+    tags_set = json.loads(json_acceptable_string)
+    tags_set['location'] = location
+
+    # Establish connection with InfluxDB
+    print("Establishing connection to InfluxDB database... ", end="")
+    client = InfluxDBClient(db_addr, db_port, 'root', 'root', db_name)
+    print("Done.")
+
+    while not killer.kill_now:
         try:
             result = urllib2.urlopen(url).read()
             data = json.loads(result)
@@ -38,18 +66,23 @@ def record_weather(api_key, latitude, longitude, db_addr, db_port, db_name, peri
 
         json_body = [{
             "measurement" : key,
+            "tags" : tags_set,
             "fields": {
                 "value": float(value)
             }} for key, value in data['currently'].items() if isfloat(value)]
 
         print("Sending to InfluxDB: \n" + str(json_body))
-        print("Result: ")
+        print("Write success: ", end="")
         print(client.write_points(json_body))
-
-        print("Complete")
+        print("Measurement complete")
+        print("Sleeping for %d seconds..." % period)
         print()
+        sys.stdout.flush()
 
-        time.sleep(period)
+        # Sleep in short bursts, so that we may exit gracefully
+        sleep_start = time.time()
+        while time.time() - sleep_start < period and not killer.kill_now:
+            time.sleep(1)
 
 def get_required_env(name):
     variable = os.environ.get(name)
@@ -63,6 +96,7 @@ def main():
     api_key = get_required_env("API_KEY")
     latitude = get_required_env("LATITUDE")
     longitude = get_required_env("LONGITUDE")
+    location = get_required_env("LOCATION")
 
     # Optional
     db_addr = os.getenv("INFLUXDB_ADDRESS", 'influxdb')
@@ -70,9 +104,10 @@ def main():
     db_name = os.getenv("INFLUXDB_NAME", 'weather')
     period = int(os.getenv("PERIOD", 120))
     units = os.getenv("UNITS", "us")
+    tags = os.getenv("TAGS", "{}")
 
     print("Entering main loop...")
-    record_weather(api_key, latitude, longitude, db_addr, db_port, db_name, period, units)
+    record_weather(api_key, latitude, longitude, db_addr, db_port, db_name, period, units, location, tags)
 
 
 if __name__ == "__main__":
